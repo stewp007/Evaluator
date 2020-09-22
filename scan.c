16af80e32157761b6288ce85670a2e2d4b001e92
Stewart Powell <sgpowell@gmail.com>
2020-09-22 03:48:50 +0100

    Added comments to scan.c and implemented all necessary requirements for project02

--- scan.c
+++ scan.c
@@ -4,16 +4,25 @@
 
 char *scan_token_strings[] = SCAN_TOKEN_STRINGS;
 
+/*
+* prints out an error message when scanning
+*/
 void scan_error(char *err) {
     printf("scan_error: %s\n", err);
     exit(-1);
 }
 
+/*
+* initializes the scan_table_st struct
+*/
 void scan_table_init(struct scan_table_st *st) {
     st->len = 0;
     st->next = 0;
 }
 
+/*
+* creates a new scan token and adds it to the scan table
+*/
 struct scan_token_st * scan_table_new_token(struct scan_table_st *st) {
     struct scan_token_st *tp;
 
@@ -23,10 +32,12 @@ struct scan_token_st * scan_table_new_token(struct scan_table_st *st) {
     return tp;
 }
 
+/*
+* Reads a token starting at p for len characters.
+*  Update the given token with the token string and token id.
+*/
 char * scan_read_token(struct scan_token_st *tp, char *p, int len,
                        enum scan_token_enum id) {
-    /* Read a token starting a p for len characters.
-       Update the given token with the token string and token id. */
     int i;
 
     tp->id = id;
@@ -38,25 +49,42 @@ char * scan_read_token(struct scan_token_st *tp, char *p, int len,
     return p;
 }
 
+/*
+* Checks wether a char is a digit character
+*/
 bool scan_is_digit(char c) {
     return c >= '0' && c <= '9';
 }
 
+/*
+* Checks wether a char is a hexadecimal letter character
+*/
 bool scan_is_hex_letter(char c){
 	return (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
 }
 
+/*
+* Checks wether a char is a whitespace character
+*/
 bool scan_is_whitespace(char c) {
     return (c == ' ' || c == '\t');
 }
 
+/*
+* scans over whitespace starting and p and 
+* finishing either when no more whitespace or reached end
+*/
 char * scan_whitespace(char *p, char *end) {
     while (scan_is_whitespace(*p) && (p < end)) {
         p += 1;
     }
     return p;
 }
-    
+
+/*
+* Scans through p until either no more integers or reached end. 
+* Updates value with each found integer
+*/
 char * scan_integer(char *p, char *end, struct scan_token_st *tp) {
     int i = 0;
     while (scan_is_digit(*p) && p < end) {
@@ -69,14 +97,23 @@ char * scan_integer(char *p, char *end, struct scan_token_st *tp) {
     return p;
 }
 
+/*
+* Checks wether a char is a binary digit character
+*/
 bool scan_is_bin_digit(char c) {
     return c == '0' || c ==  '1';
 }
 
+/*
+* Checks wether a char is a hexadecimal digit character (number or letter)
+*/
 bool scan_is_hex_digit(char c) {
     return scan_is_digit(c) || scan_is_hex_letter(c);
 }
 
+/*
+* Scans through p to create a binary scan token
+*/
 char *scan_binary_literal(char *p, char *end, struct scan_token_st *tp) {
     int i = 0;
     while (scan_is_bin_digit(*p) && p < end) {
@@ -88,6 +125,10 @@ char *scan_binary_literal(char *p, char *end, struct scan_token_st *tp) {
     tp->id = TK_BINLIT;
     return p;
 }
+
+/*
+* Scans through p to create a hexadecimal scan token
+*/
 char *scan_hex_literal(char *p, char *end, struct scan_token_st *tp) {
     int i = 0;
     while (scan_is_hex_digit(*p) && p < end) {
@@ -100,6 +141,9 @@ char *scan_hex_literal(char *p, char *end, struct scan_token_st *tp) {
     return p;
 }
 
+/*
+* Scans through p to create and add different scan tokens to the scan table
+*/
 char * scan_token(char *p, char *end, struct scan_token_st *tp) {
     if (p == end) {
         p = scan_read_token(tp, p, 0, TK_EOT);
@@ -145,6 +189,10 @@ char * scan_token(char *p, char *end, struct scan_token_st *tp) {
     return p;
 }
 
+/*
+* Scans throught the program input and creates the scan tokens, stopping once 
+* TK_EOT is created and all of the input has been scanned
+*/
 void scan_table_scan(struct scan_table_st *st, char *input, int len) {
     struct scan_token_st *tp;
     char *p = input;
@@ -159,6 +207,7 @@ void scan_table_scan(struct scan_table_st *st, char *input, int len) {
     }
 }
 
+/* pretty printing of the scan_token_table structure once scanning is completed */
 void scan_token_print(struct scan_token_st *t) {
     printf("%s(\"%s\")\n", scan_token_strings[t->id], t->value);
 }
@@ -171,10 +220,16 @@ void scan_table_print(struct scan_table_st *st) {
     }
 }
 
+/*
+* gets a scan token from the scan table
+*/
 struct scan_token_st * scan_table_get(struct scan_table_st *st, int i) {
     return &(st->table[st->next + i]);
 }
 
+/*
+* Returns true or false if the scan token gotten from the scan table is not the type of token expected
+*/
 bool scan_table_accept(struct scan_table_st *st, enum scan_token_enum tk_expected) {
     struct scan_token_st *tp;
     enum scan_token_enum tk_input;
